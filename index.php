4aec7dbd7147d386b15206668747bd2d1ccf4f6b
kou1153 <71371833+kou1153@users.noreply.github.com>
2021-09-30 21:26:31 +0700

    test

---
+++ index.php
@@ -0,0 +1,165 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Stream TV and Movies-Hulu</title>
+</head>
+<body>
+    <header class="header">
+        <nav>
+            <ul>
+                <li>
+                    <button class="login-btn">LOG IN</button>
+                </li>  
+            </ul>
+        </nav>
+        <div class="header-content">
+            <h4>Try up to one month free</h4>
+            <img src="./img/logo.png" alt="" srcset="" class="logo">
+            <div class="header-text-1">
+                <h3 style="font-weight: 600;">Watch thousands of TV shows and movies.</h3>
+            </div>
+            <div class="header-text-2">
+                <h5 style="font-weight: 400;">HBO Max™, SHOWTIME<sup>®</sup>,CINEMAX<sup>®</sup> and STARZ<sup>®</sup>available as add-ons.</h5>
+            </div>
+            <button class="btn btn-cta">
+                Start your free trail
+            </button>
+            <div class="legal-text">
+                <p>Free trial for new & eligible returning subscribers only.</p>
+            </div>
+        </div>
+    </header>
+    <section class="sub-header">
+        <img src="./img/logos.png" alt="" srcset="">
+        <div class="">
+            <h4>BUNDLE WITH ANY HULU PLAN & SAVE</h4>
+            <h3>Get Hulu, Disney+, and ESPN+.</h3>
+            <a style="text-decoration: underline; color: #fff; opacity: 0.7; font-weight: lighter;" href="#" class="sub-link">Details</a>
+        </div>
+        <div>
+            <a  class="btn btn-outline">Get Bundle</a>
+            <a style="text-decoration: underline;" href="" class="sub-link">Terms apply</a>
+        </div>
+    </section>
+    <section class="categories">
+        <h4>INCLUDED IN ALL PLANS</h4>
+        <div class="text-xl">All The TV You Love</div>
+        <div class="sub-text">
+            Stream full seasons of exclusive series, current-season episodes, hit movies, Hulu Originals, kids shows, and more.
+        </div>
+        <div class="covers">
+            <div class="cover-1">
+               <div class="cover-grad"></div>
+               <div class="cover-text">
+                   <div class="sub-title">Past & Current Seasons</div>
+                   <h3>TV Shows</h3>
+               </div>
+            </div>
+
+            <div class="cover-2">
+               <div class="cover-grad"></div>
+               <div class="cover-text">
+                   <div class="sub-title">New & Classics</div>
+                   <h3>Movies</h3>
+               </div>
+            </div>
+
+            <div class="cover-3">
+               <div class="cover-grad"></div>
+               <div class="cover-text">
+                   <div class="sub-title">Groundbreaking</div>
+                   <h3>Hulu Originals</h3>
+               </div>
+            </div>
+
+            <div class="cover-4">
+               <div class="cover-grad"></div>
+               <div class="cover-text">
+                   <div class="sub-title">Add-On</div>
+                   <h3>Premiums</h3>
+               </div>
+            </div>
+        </div>
+    </section>
+    <footer class="footer">
+        <div class="footer-container">
+            <div class="footer-lists">
+         <ul>
+            <li class="list-head">BROWSE</li>
+            <li><a href="#">Streaming Library</a></li>
+            <li><a href="#">Live TV</a></li>
+            <li><a href="#">Live News</a></li>
+            <li><a href="#">Live Sports</a></li>
+          </ul>
+          <ul>
+            <br>
+            <li><a href="#">TV Shows</a></li>
+            <li><a href="#">Movies</a></li>
+            <li><a href="#">Originals</a></li>
+            <li><a href="#">Networks</a></li>
+            <li><a href="#">Kids</a></li>
+            <li><a href="#">FX on Hulu</a></li>
+          </ul>
+          <ul>
+            <br>
+            <li><a href="#">Hulu, Disney+, and ESPN+</a></li>
+            <li><a href="#">Disney Bundle</a></li>
+            <li><a href="#">HBO Max</a></li>
+            <li><a href="#">Cinimax</a></li>
+            <li><a href="#">Showtime</a></li>
+            <li><a href="#">STARZ</a></li>
+          </ul>
+          <ul>
+            <li class="list-head">HELP</li>
+            <li><a href="#">Account & Billing</a></li>
+            <li><a href="#">Plans & Pricing</a></li>
+            <li><a href="#">Supported Devices</a></li>
+            <li><a href="#">Accesibility</a></li>
+          </ul>
+          <ul>
+            <li class="list-head">ABOUT US</li>
+            <li><a href="#">Press</a></li>
+            <li><a href="#">Jobs</a></li>
+            <li><a href="#">Contact</a></li>
+          </ul>
+            </div>
+            <div class="divider"></div>
+            <div class="social-icons">
+                <a href="#"><img src="./img/facebook.svg" alt=""></a>
+                <a href="#"><img src="./img/twitter.svg" alt=""></a>
+                <a href="#"><img src="./img/youtube.svg" alt=""></a>
+                <a href="#"><img src="./img/instagram.svg" alt=""></a>
+            </div>
+        </div>
+    </footer>
+    <div class="modal">
+        <div class="modal-box">
+            <div class="modal-body">
+                <h3>Log In</h3>
+                <form action="" method="post">
+                    <div class="form-control">
+                        <label for="email">Email</label>
+                        <input type="email" name="email" id="email">
+                    </div>
+
+                    <div class="form-control">
+                        <label for="password">Password</label>
+                        <input type="password" name="password" id="password">
+                    </div>
+                    <p><a href="#">Forgot your email or password</a></p>
+                     <button class="btn btn-dark">Log In</button>
+                </form>
+            </div>
+            <div class="modal-footer">
+                <p>Don't have an account? <a href="#">Start your free trial</a> </p>
+            </div>
+            <img class="close" src="./img/close.svg" alt="close" srcset="">
+        </div>
+    </div>
+    <script src="./js/main.js"></script>
+</body>
+</html>
