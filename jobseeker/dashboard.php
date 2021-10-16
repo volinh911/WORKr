c22d2f81a6b871f5750d93bd6b989d80b93c6c8d
Acesher <jack.phuc.enderking@gmail.com>
2021-10-16 21:19:59 +0700

    cleaning a speck
    of dust ig

--- jobseeker/dashboard.php
+++
@@ -1,5 +0,0 @@
-<?php include("../includes/head.php"); ?>
-    <link rel="stylesheet" href="../css/jseeker.css">
-<?php include("../includes/headd.php"); ?>
-    
-<?php include("../includes/foott.php"); ?>
