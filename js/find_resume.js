19ccb1e6999a281079c1f3baf5b2f857a60d3928
Thu Tran 19bit <1959039@itec.hcmus.edu.vn>
2021-10-18 16:42:39 +0700

    Push adjustm. of comp. detail and the remaining stuffs

---
+++ js/find_resume.js
@@ -0,0 +1,69 @@
+// this is for the dropdown list
+const selectedAll = document.querySelectorAll(".selected");
+
+selectedAll.forEach((selected) => {
+  const optionsContainer = selected.previousElementSibling;
+  const searchBox = selected.nextElementSibling;
+
+  const optionsList = optionsContainer.querySelectorAll(".option");
+
+  selected.addEventListener("click", () => {
+    if (optionsContainer.classList.contains("active")) {
+      optionsContainer.classList.remove("active");
+    } else {
+      let currentActive = document.querySelector(".options-container.active");
+
+      if (currentActive) {
+        currentActive.classList.remove("active");
+      }
+
+      optionsContainer.classList.add("active");
+    }
+
+    // searchBox.value = "";
+    filterList("");
+
+    // if (optionsContainer.classList.contains("active")) {
+    //   searchBox.focus();
+    // }
+  });
+
+  optionsList.forEach((o) => {
+    o.addEventListener("click", () => {
+      selected.innerHTML = o.querySelector("label").innerHTML;
+      optionsContainer.classList.remove("active");
+    });
+  });
+
+  // searchBox.addEventListener("keyup", function (e) {
+  //   filterList(e.target.value);
+  // });
+
+  const filterList = (searchTerm) => {
+    searchTerm = searchTerm.toLowerCase();
+    optionsList.forEach((option) => {
+      let label = option.firstElementChild.nextElementSibling.innerText.toLowerCase();
+      if (label.indexOf(searchTerm) != -1) {
+        option.style.display = "block";
+      } else {
+        option.style.display = "none";
+      }
+    });
+  };
+});
+
+//end of dropdown list
+
+//this is for the pagination
+$(document).ready(function(){
+  $('.next').click(function () {
+    $('.pagination').find('.pageNumber.active').next().addClass('active');
+    $('.pagination').find('.pageNumber.active').prev().removeClass('active');
+    })
+    $('.prev').click(function () {
+    $('.pagination').find('.pageNumber.active').prev().addClass('active');
+    $('.pagination').find('.pageNumber.active').next().removeClass('active');
+    })
+})
+
+//end of pagination
