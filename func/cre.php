988d2d33e6f0ca95af209090ed27745197a4f5fd
kou1153 <71371833+kou1153@users.noreply.github.com>
2021-11-08 21:44:35 +0700

    fix path

--- func/cre.php
+++ func/cre.php
@@ -1,6 +1,6 @@
 <?php 
 
-	require $_SERVER['DOCUMENT_ROOT'].'../vendor/autoload.php';
+	require $_SERVER['DOCUMENT_ROOT'].'/vendor/autoload.php';
 	use Cloudinary\Configuration\Configuration;
 	use Cloudinary\Api\Upload\UploadApi;
 
